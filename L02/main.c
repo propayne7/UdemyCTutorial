160db204e46ce4e0a86de5bf65f29edaccbe9722
Chris Payne <chrismpayne@outlook.com>
2020-07-22 19:22:46 -0500

    cont. working on L02

---
+++ L02/main.c
@@ -0,0 +1,18 @@
+/*
+ * File: main.c
+ * Author: Chris Payne
+ *
+ * Created on 07.22.2020 1830
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ /*
+  *
+  */
+
+int main() {
+    printf("Hello World\n") ;
+    return (EXIT_SUCCESS) ;
+}
