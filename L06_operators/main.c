d3da6e4f7038829dff551d33437536ba0dc8dada
Chris Payne <chris.payne@rsmus.com>
2020-08-14 15:55:46 -0500

    added L06 directory

--- L06_operators/main.c
+++ L06_operators/main.c
@@ -1,4 +1,13 @@
-int main()
+#include <stdio.h>
+
+int main(int argc, char *argvp[])
 {
+    int number_of_arguments = argc;
+    char *arg1 = argvp[0];
+    char *arg2 = argvp[1];
+
+    printf("Number of arguments: %d\n", argc);
+    printf("arg1: %s\n", argvp[0]);
+    printf("arg2: %s\n", argvp[1]);
     return 0;
 }
